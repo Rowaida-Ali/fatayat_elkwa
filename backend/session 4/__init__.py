c45f485e7f2173514fdc17ffb7c8c1239ef9db15
Rowaida ali <rowaidaali396@gmail.com>
2024-09-13 17:48:58 +0300

    fixing signup and login backend

--- backend/session 4/__init__.py
+++ backend/session 4/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SECRET_KEY"] = "your_secret_key"
 
 db = SQLAlchemy(app)
--- backend/session 4/app.py
+++ backend/session 4/app.py
@@ -1,57 +1,51 @@
-from flask import Flask,request,session
-from models import db , User
+from flask import Flask, request, session
+from models import db, User
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
 
 
-@app.route('/registration',methods=['GET','POST'])
+@app.route("/signup", methods=["GET", "POST"])
 def registration():
-    if request.method=='POST':
-        json=request.get_json()
-        username=json["username"]
-        json2=request.get_json()
-        password=json2["password"]
-        json3=request.get_json()
-        email=json3["email"]
-    user=User(
-         email=json["email"],
-         password=json["password"],
-         username=json["username"]
-     )
- 
+    if request.method == "POST":
+        json = request.get_json()
+    user = User(
+        email=json["email"], password=json["password"], username=json["username"]
+    )
+
+    user_db_email = User.query.filter_by(email=user.email).first()
+    user_db_email_username = User.query.filter_by(username=user.username).first()
+    if len(user.password) < 6:
+        return "Password is short"
+    if not (user_db_email is None):
+        return "Email already saved"
+    if not (user_db_email_username is None):
+        return "Username is already saved"
     db.session.add(user)
     db.session.commit()
     session.get(user)
-    if len(password)<6 :
-        return'password is short'
-    # user=User.query.filter_by(email=email).first()
-    # if not (user is None):
-    #     return 'Email is already saved' 
-    else:
-          return'signed up successfully'
-    
+    return "Signed up successfully"
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method=="POST":
-        json3=request.get_json()
-        email=json3["email"]     
-        json2=request.get_json()
-        password=json2["password"]
-    user=User.query.filter_by(email=email).first()
+    if request.method == "POST":
+        json3 = request.get_json()
+        email = json3["email"]
+        json2 = request.get_json()
+        password = json2["password"]
+    user = User.query.filter_by(email=email).first()
     if user is None:
-        return 'incorrect email'
-    if user.password!=password:
-        return 'incorrect password'
-    return 'logged in'
-if __name__=='__main__':
+        return "User not found"
+    elif user.password != password:
+        return "Incorrect password or email"
+    return "Logged in successfully"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-    
