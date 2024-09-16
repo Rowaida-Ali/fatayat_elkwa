84fa842af211b81fc1a0994092cfacac2c9d9103
Onay825 <onaysameh@gmail.com>
2024-09-16 20:17:21 +0300

    login and about page

--- frontend/login/src/App.js
+++ frontend/login/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginForm from './components/LoginForm/LoginForm';
+import Aboutpage from './components/Aboutpage/aboutpage'
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
 
-function App() {
-  return (
+export default function App(){ 
+  return(
     <div>
-      <LoginForm />
+      <BrowserRouter> 
+      <Routes>
+        <Route path="/login" element={<LoginForm />}/>
+        <Route path="/aboutpage" element={<Aboutpage />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
-}; 
+  )
+};
 
-export default App;
