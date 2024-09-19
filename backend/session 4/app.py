adf09d7d4f4417a3d21ec753f160082fa2ccec6b
Rowaida ali <rowaidaali396@gmail.com>
2024-09-19 20:22:21 +0300

    changed

--- backend/session 4/app.py
+++ backend/session 4/app.py
@@ -1,31 +1,31 @@
-from flask import Flask, request, session, jsonify ,make_response
-from models import db, User, Task, Note ,Abroad_blogs
+from flask import Flask, request, session, jsonify
+from models import db, User, Task, Note, Abroad_blogs
 from flask_cors import CORS
-from flask_bcrypt import Bcrypt 
-from flask_jwt_extended import JWTManager,create_access_token,jwt_required
-# from flask_login import UserMixin
-# import jwt
-# from datetime import datetime , timedelta
-# from functools import wraps
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    get_jwt_identity,
+)
+import jwt
+
 app = Flask(__name__)
 CORS(app)
-jwt= JWTManager(app)
+jwt = JWTManager(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///apps.db"
-app.config["SECRET_KEY"]="secret_key"
+app.config["SECRET_KEY"] = "secret_key"
 db.init_app(app)
 bcrypt = Bcrypt(app)
 
 with app.app_context():
     db.create_all()
 
-CURRENT_USER = "" 
-users=[]
-
 
 @app.route("/signup", methods=["POST"])
 def registration():
     json = request.get_json()
-    hashed_password = bcrypt.generate_password_hash(json["password"]).decode('utf-8')
+    hashed_password = bcrypt.generate_password_hash(json["password"]).decode("utf-8")
     user = User(
         email=json["email"],
         password_hash=hashed_password,
@@ -37,16 +37,12 @@ def registration():
     user_db_email = User.query.filter_by(email=user.email).first()
     user_db_email_username = User.query.filter_by(username=user.username).first()
     if not (user_db_email is None):
-        return jsonify ("Email already saved")
+        return jsonify("Email already saved"), 409
     if not (user_db_email_username is None):
-        return jsonify( "Username is already saved ")
+        return jsonify("Username is already saved "), 409
     db.session.add(user)
     db.session.commit()
-    session.get(user)
-    users.append(user.email)
-    users.append(hashed_password)
-    return jsonify("Signed up successfully")
-
+    return jsonify("Signed up successfully"), 200
 
 
 @app.route("/login", methods=["POST"])
@@ -54,13 +50,13 @@ def login():
     json = request.get_json()
     email = json["email"]
     password = json["password"]
-    if email in users and bcrypt.check_password_hash(password.encode('utf-8'),):
+    user = User.query.filter_by(email=email).first()
+    if user and bcrypt.check_password_hash(user.password_hash, password):
         access_token = create_access_token(identity=email)
         return jsonify(access_token=access_token), 200
     else:
-        print(f"user {users}")
         return jsonify({"msg": "Invalid email or password"}), 401
- 
+
 
 @app.route("/search_user", methods=["GET"])
 def search():
@@ -85,11 +81,8 @@ def search():
 @app.route("/add_task", methods=["POST"])
 def add():
     json = request.get_json()
-    # check if current_user == "" --> return jsonify"unauthorized"
-    # username = json["username"]
-    if CURRENT_USER=="":
-        return jsonify("unauthorized")
-    user = User.query.filter_by(username=CURRENT_USER).first()
+    username = json["username"]
+    user = User.query.filter_by(username=username).first()
     work = Task(your_work=json["your_work"], user_id=user.id)
     db.session.add(work)
     db.session.commit()
@@ -107,12 +100,23 @@ def remove_task():
         db.session.delete(task)
         db.session.commit()
         return jsonify("Task Deleted")
-    else:
-        return jsonify("Not Found")
+    return jsonify("Not Found"), 404
+
+
+@app.route("/edit_task", methods=["PUT"])
+def edit_tasks():
+    json = request.get_json()
+    username = json["username"]
+    your_work = json["your_work"]
+    edit_task = Task(your_work=your_work)
+    db.session.commit()
+    return jsonify("edited")
 
 
 @app.route("/view_tasks", methods=["GET"])
+@jwt_required()
 def view_tasks():
+    current_user = get_jwt_identity()
     json = request.get_json()
     username = json["username"]
     user = User.query.filter_by(username=username).first()
@@ -132,8 +136,7 @@ def task_completed():
         task.completed = True
         db.session.commit()
         return jsonify("Task marked as completed")
-    else:
-        return jsonify("task not found")
+    return jsonify("task not found"), 404
 
 
 @app.route("/take_note", methods=["POST"])
@@ -141,10 +144,23 @@ def taking_notes():
     json = request.get_json()
     username = json["username"]
     user = User.query.filter_by(username=username)
-    note = Note(your_note=json["your_note"], secret=json["secret"],  username = json["username"])
+    note = Note(
+        your_note=json["your_note"], secret=json["secret"], username=json["username"]
+    )
     db.session.add(note)
     db.session.commit()
     return jsonify("Note added")
+
+
+@app.route("/edit_note", methods=["PUT"])
+def edit_note():
+    json = request.get_json()
+    username = json["username"]
+    edit = Task(your_note=json["your_note"], secret=json["secret"])
+    user = User.query.filter_by(username=username)
+    db.session.add(edit)
+    db.session.commit()
+    return jsonify("edited")
 
 
 @app.route("/get_note_name", methods=["GET"])
@@ -154,10 +170,9 @@ def view():
     username = json["username"]
     user = User.query.filter_by(username=username).first()
     note = Note.query.filter_by(secret=secret, user_id=user.id)
-    if secret==False:
+    if secret == False:
         return jsonify("all your notes")
-    else:
-        return jsonify("not found")
+    return jsonify("not found")
 
 
 @app.route("/get_all_notes", methods=["GET"])
@@ -169,70 +184,81 @@ def get_notes():
     return jsonify({"all your notes": lst})
 
 
-@app.route('/add_blog',methods=["POST"])
+@app.route("/add_blog", methods=["POST"])
 def add_blogs():
-    json=request.get_json()
-    blogs= Abroad_blogs( username=json["username"],university=json["university"],country=json["country"],blog=json["blog"],resources=json["resources"])
+    json = request.get_json()
+    blogs = Abroad_blogs(
+        username=json["username"],
+        university=json["university"],
+        country=json["country"],
+        blog=json["blog"],
+        resources=json["resources"],
+    )
     db.session.add(blogs)
     db.session.commit()
     return jsonify("added")
 
-@app.route('/edit_blogs', methods=["PUT"])
+
+@app.route("/edit_blogs", methods=["PUT"])
 def edit():
-    json=request.get_json()
-    # blog=json["blog"]
-    blog_edited=Abroad_blogs( blog=json["blog"] )
-    db.session.add(blog_edited)
+    json = request.get_json()
+    blog = json["blog"]
+    blog_edited = Abroad_blogs.query.get(blog)
+    # blog_edited=json["blog"]
+    # db.session.add(blog)
+    # db.session.delete(blog_edited)
     db.session.commit()
-    return"done"
-    
+    return jsonify("Edited")
+
 
 @app.route("/remove_blog", methods=["DELETE"])
 def remove_blog():
-    json=request.get_json()
-    blog_removed=json["blog_removed"]
-    removed=Abroad_blogs.query.filter_by(blog=blog_removed).first()
+    json = request.get_json()
+    blog_removed = json["blog_removed"]
+    removed = Abroad_blogs.query.filter_by(blog=blog_removed).first()
     if removed:
         db.session.delete(removed)
         db.session.commit()
-        return jsonify("task removed")
-    else:
-        return jsonify("not found")
+        return jsonify("Bloged removed")
+    return jsonify("not found"), 404
+
 
 @app.route("/view_all_blogs", methods=["GET"])
 def view_blog():
     view_blogs = Abroad_blogs.query.all()
     lst = []
-    for blog in view_blogs :
-        blogs_view = { 
+    for blog in view_blogs:
+        blogs_view = {
             # 'id' : blog.id,
-            'username': blog.username,
-            'title':blog.title,
-            'blog':blog.blog,
-            'country' : blog.country,
-            'university':blog.university,
-            'resources' :blog.resources
+            "username": blog.username,
+            "title": blog.title,
+            "blog": blog.blog,
+            "country": blog.country,
+            "university": blog.university,
+            "resources": blog.resources,
         }
         lst.append(blogs_view)
     return jsonify(lst)
 
+
 @app.route("/view_my_blogs", methods=["GET"])
 def my_blogs():
-    json=request.get_json()
+    json = request.get_json()
     username = json["username"]
     blogs = Abroad_blogs.query.filter_by(username=username).all()
     lst = []
     for blog in blogs:
-        blogs_view = { 
+        blogs_view = {
             # 'id' : blog.id,
-            'title':blog.title,
-            'blog':blog.blog,
-            'country' : blog.country,
-            'university':blog.university,
-            'resources' :blog.resources
+            "title": blog.title,
+            "blog": blog.blog,
+            "country": blog.country,
+            "university": blog.university,
+            "resources": blog.resources,
         }
         lst.append(blogs_view)
     return jsonify(lst)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3003)
--- backend/session 4/models.py
+++ backend/session 4/models.py
@@ -7,7 +7,7 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 
-class User(db.Model , UserMixin):
+class User(db.Model, UserMixin):
     __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -22,7 +22,8 @@ class User(db.Model , UserMixin):
     notes = db.relationship(
         "Note", backref="User", primaryjoin="User.id == Note.user_id"
     )
-    
+
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     your_work = db.Column(db.String(100), nullable=False)
@@ -42,11 +43,11 @@ class Note(db.Model):
 
 
 class Abroad_blogs(db.Model):
-    id=db.Column (db.Integer,primary_key=True)
-    country=db.Column(db.String(50))
-    university=db.Column(db.String(100))
-    blog=db.Column(db.Text(5000))
-    resources=db.Column(db.String(200))
-    username=db.Column(db.String(50))
-    title = db.Column (db.String(150))
+    id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String(50))
+    university = db.Column(db.String(100))
+    blog = db.Column(db.Text(5000))
+    resources = db.Column(db.String(200))
+    username = db.Column(db.String(50))
+    title = db.Column(db.String(150))
     # user_id = db.Column(db.Integer, db.ForeignKey("User.id"))
