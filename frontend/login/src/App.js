c86eb1a89345ffc007d3c85bc96824d071705e0f
MernaWessa <mirnawessa@gmail.com>
2024-09-29 14:04:38 +0300

    accept updates

--- frontend/login/src/App.js
+++ frontend/login/src/App.js
@@ -4,7 +4,10 @@ import LoginForm from './components/LoginForm/LoginForm';
 import Aboutpage from './components/Aboutpage/aboutpage';
 import SignupForm from './components/SignupForm/SignupForm';
 import HomePage from './components/Homepage/homepage';
-import StudingAbroad from './components/StudingAbroad/StudigAbroad'
+import StudingAbroad from './components/StudingAbroad/StudigAbroad';
+import CreateNote from './components/CreateNote/NoteTaker';
+import ViewTakingNotes from './components/viewTakingNotes/ViewNotesPrivate';
+import ViewTakingNotes from './components/viewTakingNotes/viewTakingNotes';
 import{BrowserRouter,Routes,Route} from 'react-router-dom';
 
 export default function App(){ 
@@ -16,6 +19,8 @@ export default function App(){
         <Route path="/aboutpage" element={<Aboutpage />}/>
         <Route path="/signup" element={<SignupForm />}/>
         <Route path='/home' element={<HomePage />}/>
+        <Route path='/view_notes' element={<ViewTakingNotes />}/>
+        <Route path='/createnote' element={<CreateNote />}/>
         <Route path="/studingabroad" element={<StudingAbroad />}/>
       </Routes>
       </BrowserRouter>
